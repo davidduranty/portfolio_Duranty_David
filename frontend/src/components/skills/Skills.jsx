d4ec56919545132fbb413ee4c1672ef26cd40401
David Duranty <dadaduranty@gmail.com>
2024-01-05 08:13:34 +0100

    add components skills

---
+++ frontend/src/components/skills/Skills.jsx
@@ -0,0 +1,34 @@
+function Skills() {
+  return (
+    <div className="skills container">
+      <div className="front-skills-container">
+        <p>Front-end</p>
+        <li>HTML5</li>
+        <li>CSS / SASS</li>
+        <li>REACT JS</li>
+        <li>NODE JS</li>
+        <li>GITHUB</li>
+      </div>
+      <div className="back-skills-container">
+        <p>Back-end</p>
+        <li>EXPRESS</li>
+        <li>MYSQL</li>
+        <li>DBEAVER</li>
+      </div>
+      <div className="tool-skills-container">
+        <p>Outils</p>
+        <li>FIGMA</li>
+        <li>TRELLO</li>
+      </div>
+      <div className="more-skills-container">
+        <p>Soft Skills</p>
+        <li>Sens du collectif</li>
+        <li>Autonomie</li>
+        <li>Goût de l'effort</li>
+        <li>Apprentissage actif</li>
+      </div>
+    </div>
+  );
+}
+
+export default Skills;
