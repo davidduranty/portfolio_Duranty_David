05676c0cfab17cb2e9bceb23a6e89dd9493dce80
David Duranty <dadaduranty@gmail.com>
2024-01-06 12:01:20 +0100

    add components footer

--- frontend/src/components/footer/Footer.jsx
+++ frontend/src/components/footer/Footer.jsx
@@ -1,5 +1,22 @@
+import { Link } from "react-router-dom";
+
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <div className="footer-container">
+      <h3>David Duranty</h3>
+      <div className="icon-footer">
+        <Link to="https://github.com/davidduranty">
+          <img src="github.png" alt="" />
+        </Link>
+        <li>
+          <a href="mailto:chrisdavd@gmail.com">Email</a>
+        </li>
+        <Link to="https://www.linkedin.com/in/david-duranty/">
+          <img src="linkedin.png" alt="" />
+        </Link>
+      </div>
+    </div>
+  );
 }
 
 export default Footer;
