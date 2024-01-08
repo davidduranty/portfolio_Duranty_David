6629c7d42c246e1e2e33cea5fe7e633d563fde2c
David Duranty <dadaduranty@gmail.com>
2024-01-08 22:38:21 +0100

    add components project 2

---
+++ frontend/src/components/Technology/TechnologyProject2.jsx
@@ -0,0 +1,28 @@
+function TechnologyProject2() {
+  return (
+    <>
+      <h1 className="title-tech">Les technologies utilisées</h1>
+      <div className="container-tech">
+        <div className="project-tech">
+          <p className="text-title">Front-end</p>
+          <li>Html 5</li>
+          <li>Sass</li>
+          <li>Javascript</li>
+          <li>Réact Js</li>
+        </div>
+        <div className="project-tech">
+          <p className="text-title">Back-end</p>
+          <li>Express Js</li>
+        </div>
+        <div className="project-tech">
+          <p className="text-title">Outils</p>
+          <li>Figma</li>
+          <li>Trello</li>
+          <li>Github</li>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default TechnologyProject2;
