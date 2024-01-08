f452e871a8361aea6a035a95d723c3fbe7acac65
David Duranty <dadaduranty@gmail.com>
2024-01-08 13:31:17 +0100

    add components projects 2

---
+++ frontend/src/components/Projects-header/HeaderProject2.jsx
@@ -0,0 +1,10 @@
+function HeaderProject2() {
+  return (
+    <div className="project-info-container">
+      <h1>Studio Gihbli</h1>
+      <p>Site de films du studio Gihbli</p>
+    </div>
+  );
+}
+
+export default HeaderProject2;
