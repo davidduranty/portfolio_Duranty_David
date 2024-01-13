d776bc6114fb82f3502616a3da4b95ff9fb90ac8
David Duranty <dadaduranty@gmail.com>
2024-01-13 02:32:14 +0100

    adjust scss home page

--- frontend/src/components/projects-files/Projects.jsx
+++ frontend/src/components/projects-files/Projects.jsx
@@ -7,17 +7,18 @@ function Projects() {
       <div className="projects-container">
         <div className="project-1 project-df">
           <img className="project" src="wild-social.png" alt="Projet 1" />
+        </div>
+        <div className="btn-link">
           <Link to="/project1">
             <button className="btn">Wild social</button>
           </Link>
         </div>
-        <div
-          className="project-2 project-df
-         "
-        >
+        <div className="project-2 project-df">
           <img className="project" src="studio.png" alt="Projet 2" />
-          <Link to="/project2">
-            <button className="btn">Studio Gihbli</button>
+        </div>
+        <div className="btn-link">
+          <Link className="btn" to="/project2">
+            <button>Studio Gihbli</button>
           </Link>
         </div>
         <div
@@ -25,8 +26,10 @@ function Projects() {
          "
         >
           <img className="project" src="birdies.png" alt="Projet-3" />
+        </div>
+        <div className="btn-link">
           <Link to="/project3">
-            <button className="btn">Birdies</button>
+            <button>Birdies</button>
           </Link>
         </div>
       </div>
