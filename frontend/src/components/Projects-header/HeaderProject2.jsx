9ee455d20b6cf3e2ffdba3a63fcc34d42c1a045f
David Duranty <dadaduranty@gmail.com>
2024-01-14 14:25:47 +0100

    adjust project 2 classname and scss

--- frontend/src/components/Projects-header/HeaderProject2.jsx
+++ frontend/src/components/Projects-header/HeaderProject2.jsx
@@ -1,14 +1,21 @@
+import { Link } from "react-router-dom";
+
 function HeaderProject2() {
   return (
-    <div className="project-info-container">
-      <img
-        className="img-info"
-        src="capture_ghibli.png"
-        alt="image studio Ghibli"
-      />
-      <div className="project-title-container">
-        <h1>Studio Gihbli</h1>
-        <p>Site de films du studio Gihbli</p>
+    <div className="container">
+      <div className="project-info-container">
+        <img
+          className="img-info"
+          src="capture_ghibli.png"
+          alt="image studio Ghibli"
+        />
+        <div className="project-title-container">
+          <h1>Studio Gihbli</h1>
+          <p>Site de films du studio Gihbli</p>
+        </div>
+        <Link to="" target="_blank">
+          <p className="btn-project">Studio Ghibli</p>
+        </Link>
       </div>
     </div>
   );
--- frontend/src/components/Projects-skills/SkillsProject2.jsx
+++ frontend/src/components/Projects-skills/SkillsProject2.jsx
@@ -4,29 +4,36 @@ import CV_David from "../../pages/CV_David";
 function SkillsProject2() {
   const filePath = "/" + `cv.pdf`;
   return (
-    <div className="container-about-project">
-      <div className="content-about-project">
-        <h1>A propos</h1>
-        <p>
-          Le projet avait pour but de créer un site statique avec le framework
-          Réact Js en faisant appel à une API pour traiter les réponses. Cette
-          exercice à permis d aller plus loin dans la gestion du projet. Tout
-          ceci a donné l occasion de se rendre compte de la puissance de ce
-          framework.
-        </p>
-        <Link to={filePath} target="_blank">
-          <button id="btn">
-            Télécharger mon CV
-            <CV_David />
-          </button>
-        </Link>
-      </div>
-      <div className="img-about-project">
-        <img
-          className="img-project"
-          src="studio.png"
-          alt="photo site studio Gihbli"
-        />
+    <div className="container">
+      <div className="container-skills">
+        <div className="content-about-project">
+          <h1>A propos</h1>
+          <p>
+            Le projet avait pour but de créer un site statique avec le framework
+            Réact Js en faisant appel à une API pour traiter les réponses. Cette
+            exercice à permis d aller plus loin dans la gestion du projet. Tout
+            ceci a donné l occasion de se rendre compte de la puissance de ce
+            framework.
+          </p>
+          <Link to={filePath} target="_blank">
+            <button id="btn">
+              Télécharger mon CV
+              <CV_David />
+            </button>
+          </Link>
+        </div>
+        <div className="container-img-project">
+          <div className="img-about-project">
+            <img
+              className="img-project"
+              src="studio.png"
+              alt="photo site studio Gihbli"
+            />
+          </div>
+          <div className="img-about-project-add">
+            <img src="capture.png" alt="" />
+          </div>
+        </div>
       </div>
     </div>
   );
