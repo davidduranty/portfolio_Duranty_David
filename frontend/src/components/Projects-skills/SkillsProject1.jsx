6df4f85ea98bfdbf891fe7659c57f87d2b4cc504
David Duranty <dadaduranty@gmail.com>
2024-01-15 22:31:53 +0100

    adjust project 2 responsive

--- frontend/src/components/Projects-skills/SkillsProject1.jsx
+++ frontend/src/components/Projects-skills/SkillsProject1.jsx
@@ -1,40 +1,71 @@
 import { Link } from "react-router-dom";
 import CV_David from "../../pages/CV_David";
+import { useEffect } from "react";
 
 function SkillsProject1() {
   const filePath = "/" + `cv.pdf`;
-  return (
-    <div className="container">
-      <div className="container-skills">
-        <div className="content-about-project">
-          <h1>A propos</h1>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum
-            architecto, ea ex nobis quam labore nemo. Tenetur, quo sit optio
-            totam iusto eius. Ad vero voluptate corporis repudiandae laudantium
-            dolore nemo, magnam enim cum mollitia, id alias delectus deleniti
-            soluta sed .
-          </p>
-        </div>
-        <div className="btn-project">
-          <Link to={filePath} target="_blank">
-            <button id="btn">
-              Télécharger mon CV
-              <CV_David />
-            </button>
-          </Link>
-        </div>
 
-        <div className="container-img-project">
-          <div className="img-about-project">
-            <img
-              className="img-project"
-              src="wildsocial.png"
-              alt="photo site wild social"
-            />
+  useEffect(() => {
+    const handleScroll = () => {
+      const skillsBoxesElement = document.querySelector(
+        ".container-skills-boxes"
+      );
+      const imgProjectElement = document.querySelector(
+        ".container-img-project"
+      );
+
+      if (window.scrollY > 200) {
+        skillsBoxesElement.classList.add("visible");
+      }
+
+      if (window.scrollY > 300) {
+        imgProjectElement.classList.add("visibleImg");
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+  return (
+    <div className="container-max bg-purple">
+      <div className="container">
+        <div className="container-skills">
+          <div className="container-skills-boxes">
+            <div className="content-about-project">
+              <h1>A propos</h1>
+              <p>
+                Ce projet d un réseau social permet de pouvoir se créer un
+                profil utilisateur afin de pouvoir accéder à l application. Le
+                HTML et CSS ont permi de créer un style de qualité. Grâce au
+                Javascript, on a la possibilité de liker ou desliker un post.
+                Egalement de pouvoir laisser un commentaire sur une actualité.
+                La consultaion des derniers messages sont accessibles. Cette
+                application est très fonctionnelle pour l utilisateur.
+              </p>
+            </div>
+            <div className="btn-project">
+              <Link to={filePath} target="_blank">
+                <button id="btn">
+                  Télécharger mon CV
+                  <CV_David />
+                </button>
+              </Link>
+            </div>
           </div>
-          <div className="img-about-project-add">
-            <img src="capture.png" alt="" />
+          <div className="container-img-project">
+            <div className="img-about-project">
+              <img
+                className="img-project"
+                src="acceuil.png"
+                alt="photo site wild social"
+              />
+            </div>
+            <div className="img-about-project-add">
+              <img src="wildsocial.png" alt="" />
+            </div>
           </div>
         </div>
       </div>
