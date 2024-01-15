8956847a129b1cc147ea1a1385736bec5d68b510
David Duranty <dadaduranty@gmail.com>
2024-01-15 19:03:21 +0100

    adjust scss all components

--- frontend/src/components/footer/Footer.jsx
+++ frontend/src/components/footer/Footer.jsx
@@ -2,22 +2,31 @@ import { Link } from "react-router-dom";
 
 function Footer() {
   return (
-    <div className="container">
-      <div className="footer">
-        <h3>David Duranty</h3>
-        <p className="email">chrisdavd@gmail.com</p>
-        <div className="icon-footer">
-          <Link to="https://github.com/davidduranty" target="_blank">
-            <img src="github.png" alt="" />
-          </Link>
+    <div className="container-max bg-purple">
+      <div className="container">
+        <div className="footer">
+          <h3>David Duranty</h3>
+          <p className="email">chrisdavd@gmail.com</p>
+          <div className="icon-footer">
+            <Link to="https://github.com/davidduranty" target="_blank">
+              <img src="github.png" alt="" />
+            </Link>
 
-          <a href="mailto:chrisdavd@gmail.com" target="_blank" rel="noreferrer">
-            <img src="Letter.png" alt="image email" />
-          </a>
+            <a
+              href="mailto:chrisdavd@gmail.com"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img src="Letter.png" alt="image email" />
+            </a>
 
-          <Link to="https://www.linkedin.com/in/david-duranty/" target="_blank">
-            <img src="linkedin.png" alt="" />
-          </Link>
+            <Link
+              to="https://www.linkedin.com/in/david-duranty/"
+              target="_blank"
+            >
+              <img src="linkedin.png" alt="" />
+            </Link>
+          </div>
         </div>
       </div>
     </div>
