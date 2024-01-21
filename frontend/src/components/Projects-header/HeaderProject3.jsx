4b410be051672030a466650996b0070e90db2133
David Duranty <dadaduranty@gmail.com>
2024-01-21 09:56:05 +0100

    adjust las t element and fixed scss

--- frontend/src/components/Projects-header/HeaderProject3.jsx
+++ frontend/src/components/Projects-header/HeaderProject3.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 function HeaderProject3() {
   return (
     <div className="project-info-container">
@@ -6,6 +8,12 @@ function HeaderProject3() {
         <h1 className="title-wild">BIRDIES</h1>
         <p>Site de recettes de cuisine</p>
       </div>
+      <Link
+        to="https://github.com/WildCodeSchool/2023-09-JS-BDX-P3-Birdies/tree/dev"
+        target="_blank"
+      >
+        <p className="btn-project">Birdies</p>
+      </Link>
     </div>
   );
 }
