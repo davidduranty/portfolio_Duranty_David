c6e62539e7efdc8b9318b68a07fcda08256f20c6
David Duranty <dadaduranty@gmail.com>
2024-01-24 00:09:52 +0100

    delete arrow backToTop

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,12 @@
 import Navbar from "./components/navbar/Navbar";
 import Footer from "./components/footer/Footer";
 import { Outlet } from "react-router-dom";
-import ArrowFixed from "./components/BackToTop/ArrowFixed";
+
 function App() {
   return (
     <>
       <Navbar />
       <Outlet />
-      <ArrowFixed />
       <Footer />
     </>
   );
